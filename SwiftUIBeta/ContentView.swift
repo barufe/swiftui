7aa39ec721c9a154db1e0c94baaa6000f51de96f
barufe <belkisarufe@gmail.com>
2024-01-22 15:20:04 -0500

    initial commit

---
+++ SwiftUIBeta/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIBeta
+//
+//  Created by Germain Seijas on 22/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIBeta/SwiftUIBetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBetaApp.swift
+//  SwiftUIBeta
+//
+//  Created by Germain Seijas on 22/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
